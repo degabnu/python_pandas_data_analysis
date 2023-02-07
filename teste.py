4a2a245084756bd9098948a8e7cbc186be904d71
Douglas <degabnu@gmail.com>
2023-02-06 21:41:03 -0300

    Commit inicial.

---
+++ teste.py
@@ -0,0 +1,15 @@
+################################################################
+################################################################
+from matplotlib import pyplot as plt
+import numpy as np
+
+ys = 200 + np.random.randn(100)
+x = [x for x in range(len(ys))]
+
+plt.plot(x, ys, '-')
+plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
+
+plt.title("Teste de Gráfico")
+plt.show()
+################################################################
+################################################################
