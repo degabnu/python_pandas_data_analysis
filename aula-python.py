be1d39110780e144a4a9ceef4de38c2b2be38a7a
Douglas <degabnu@gmail.com>
2023-02-08 21:56:39 -0300

    testes com python pandas

--- aula-python.py
+++ aula-python.py
@@ -23,5 +23,27 @@ v_combustiveis_excel = pd.read_excel("ca-2021-02.xlsx")
 v_ca_df = v_combustiveis_excel[[
     'Revenda', 'Municipio', 'Produto', 'Valor de Venda']]
 v_gas_df = v_ca_df.loc[v_ca_df['Produto'] == 'GASOLINA']
-print(v_gas_df['Valor de Venda'].max())
-print(v_gas_df[['Municipio', 'Revenda', 'Valor de Venda']].max())
+# print(v_gas_df['Valor de Venda'].max())
+# print(v_gas_df[['Municipio', 'Revenda', 'Valor de Venda']].max())
+# print(v_gas_df[['Municipio', 'Revenda', 'Valor de Venda']])
+# print(v_gas_df.sort_values(by='Valor de Venda')[
+#       ['Municipio', 'Revenda', 'Valor de Venda']])
+# média de preço
+# v_municipio = v_combustiveis_excel[v_combustiveis_excel['Bairro']
+#                                    == 'BELA VISTA']
+# print(v_municipio)
+# v_municipio_loc = v_combustiveis_excel.loc[(v_combustiveis_excel['Bairro'] == 'MOOCA') & (
+#     v_combustiveis_excel['Municipio'] == 'SAO PAULO')]
+# print(v_municipio_loc)
+# v_municipio_loc2 = v_combustiveis_excel.loc[(v_combustiveis_excel['Bairro'] == 'MOOCA') & (
+#     (v_combustiveis_excel['Produto'] == 'GASOLINA') | (v_combustiveis_excel['Produto'] == 'GASOLINA ADITIVADA'))]
+# print(v_combustiveis_excel.loc[(v_combustiveis_excel['Bairro'] == 'MOOCA') & (v_combustiveis_excel['Municipio'] == 'SAO PAULO') & (
+#     (v_combustiveis_excel['Produto'] == 'GASOLINA') | (v_combustiveis_excel['Produto'] == 'GASOLINA ADITIVADA')), ['Valor de Venda']].mean())
+# v_produto = v_combustiveis_excel[[
+#     'Produto', 'Valor de Venda']].groupby(by='Produto').mean().round(2)
+# print(v_produto)
+# v_combustiveis_excel['Ativo'] = True
+# print(v_combustiveis_excel)
+# print(v_combustiveis_excel.head(10))
+
+v_ca_df.to_excel("ca-teste.xlsx", index=False)
--- teste.py
+++ teste.py
@@ -1,15 +1,16 @@
-################################################################
-################################################################
-from matplotlib import pyplot as plt
-import numpy as np
+# ################################################################
+# ################################################################
+# from matplotlib import pyplot as plt
+# import numpy as np
 
-ys = 200 + np.random.randn(100)
-x = [x for x in range(len(ys))]
+# ys = 200 + np.random.randn(100)
+# x = [x for x in range(len(ys))]
 
-plt.plot(x, ys, '-')
-plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
+# plt.plot(x, ys, '-')
+# plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
 
-plt.title("Teste de Gráfico")
-plt.show()
-################################################################
-################################################################
+# plt.title("Teste de Gráfico")
+# plt.show()
+# ################################################################
+# ################################################################
+# Qual a média de preços dos combustíveis GASOLINA e GASOLINA ADITIVADA do Bairro MOOCA em SÃO PAULO?
